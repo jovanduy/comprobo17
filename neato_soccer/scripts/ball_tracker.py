619a9e43e4d070b237467a130396d802fece2998
Jordan Van Duyne <jordannn808@gmail.com>
2017-02-28 11:52:06 -0500

    work on ball tracker

--- neato_soccer/scripts/ball_tracker.py
+++ neato_soccer/scripts/ball_tracker.py
@@ -25,32 +25,31 @@ class BallTracker(object):
 
         rospy.Subscriber(image_topic, Image, self.process_image)
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-        cv2.namedWindow('video_window')
-        cv2.setMouseCallback('video_window', self.process_mouse_event)
-        cv2.namedWindow('threshold_image')
         self.green_lower_bound = 0
+        cv2.namedWindow('video_window')
+#        cv2.setMouseCallback('video_window', self.process_mouse_event)
+        cv2.namedWindow('threshold_image')
         cv2.createTrackbar('green lower bound', 'threshold_image', 0, 255, self.set_green_lower_bound)
 
     def process_image(self, msg):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image for subsequent processing """
         self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
-        binary_image = cv2.inRange(self.cv_image, (0,self.green_lower_bound,0), (0,255,150))
-        cv2.imshow('threshold_image', binary_image)
+        self.binary_image = cv2.inRange(self.cv_image, (0,self.green_lower_bound,0), (0,255,150))
         self.hsv_image = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
 
     def process_mouse_event(self, event, x,y,flags,param):
         """ Process mouse events so that you can see the color values associated
             with a particular pixel in the camera images """
-            image_info_window = 255*np.ones((500,500,3))
-            cv2.putText(image_info_window,
+        image_info_window = 255*np.ones((500,500,3))
+        cv2.putText(image_info_window,
                         'Color (b=%d,g=%d,r=%d)' % (self.cv_image[y,x,0], self.cv_image[y,x,1], self.cv_image[y,x,2]),
                         (5,50),
                         cv2.FONT_HERSHEY_SIMPLEX,
                         1,
-                        (0, 0, 0))                                                                                                                        (0,0,0))
-            cv2.imshow('image_info', image_info_window)
-            cv2.waitKey(5)
+                        (0, 0, 0))
+        #cv2.imshow('image_info', image_info_window)
+        #cv2.waitKey(5)
 
     def set_green_lower_bound(self, val):
         """ A callback function to handle the OpenCV slider to select the green lower bound """
@@ -63,6 +62,7 @@ class BallTracker(object):
             if not self.cv_image is None:
                 print self.cv_image.shape
                 cv2.imshow('video_window', self.cv_image)
+        	cv2.imshow('threshold_image', self.binary_image)
                 cv2.waitKey(5)
 
             # start out not issuing any motor commands
